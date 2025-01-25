1015b51d9a981c80e79ff8f045bcb246fa118d7b
Amisha Choudhary <103454929+Amishachoudhary22@users.noreply.github.com>
2025-01-25 13:13:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,16 +7,16 @@ import streamlit as st
 import os
 
 # Retrieve the API key from Streamlit secrets
-API_KEY = st.secrets["secrets"]["ROBOFLOW_API_KEY"]
+#API_KEY = st.secrets["secrets"]["ROBOFLOW_API_KEY"]
 # Initialize Roboflow clients
 CLIENT = InferenceHTTPClient(
     api_url="https://outline.roboflow.com",
-    api_key=API_KEY
+    api_key=rf_mgYNYWgpLsTOl5YLHRXUJkmYEDv1
 )
 
 CLIENT2 = InferenceHTTPClient(
     api_url="https://outline.roboflow.com",
-    api_key=API_KEY
+    api_key=rf_mgYNYWgpLsTOl5YLHRXUJkmYEDv1
 )
 
 # Define class names for prediction
